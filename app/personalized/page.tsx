ba81e9e7e648591bfa9b8bb7e395b121785c9cb2
DravikKahar <dravikk5@gmail.com>
2024-11-05 14:59:02 +0530

    initial

---
+++ app/personalized/page.tsx
@@ -0,0 +1,9 @@
+import Personalized from "../components/Personalized";
+
+export default function order() {
+    return (
+        <main>
+            <Personalized></Personalized>
+        </main>
+    )
+}
